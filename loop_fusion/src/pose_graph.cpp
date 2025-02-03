bb5a296fd90fcaea7ae023906bddebc5a7eb47a1
Martin Schlodinski <martin.schlodinski@moia.io>
2025-02-03 15:03:25 +0100

    Change LocalParametrization to Manifold

--- loop_fusion/src/pose_graph.cpp
+++ loop_fusion/src/pose_graph.cpp
@@ -480,8 +480,8 @@ void PoseGraph::optimize4DoF()
             ceres::LossFunction *loss_function;
             loss_function = new ceres::HuberLoss(0.1);
             //loss_function = new ceres::CauchyLoss(1.0);
-            ceres::Manifold* angle_local_parameterization =
-                AngleLocalParameterization::Create();
+            ceres::Manifold* angle_manifold =
+                ceres::AngleManifold::Create();
 
             list<KeyFrame*>::iterator it;
 
@@ -508,7 +508,7 @@ void PoseGraph::optimize4DoF()
 
                 sequence_array[i] = (*it)->sequence;
 
-                problem.AddParameterBlock(euler_array[i], 1, angle_local_parameterization);
+                problem.AddParameterBlock(euler_array[i], 1, angle_manifold);
                 problem.AddParameterBlock(t_array[i], 3);
 
                 if ((*it)->index == first_looped_index || (*it)->sequence == 0)
